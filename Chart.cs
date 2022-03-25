2b26b15d6f8e71e733bf59ad2d70e17aba2003fd
yamaccu <yus1206@hotmail.com>
2022-03-26 01:05:56 +0900

    first

---
+++ Chart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ChartjsTest
+{
+    public class ChartJson
+    {
+        public string type { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public IEnumerable<string> labels { get; set; }
+        public Dataset[] datasets { get; set; }
+    }
+
+    public class Dataset
+    {
+        public string label { get; set; }
+        public IEnumerable<int> data { get; set; }
+        public string backgroundColor { get; set; }
+        public string borderColor { get; set; }
+        public int borderWidth { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Blazor-CharjsTest
+Using Chart.js with Blazor  
+
+<br><br>
+
+以下のページで解説しています。
+[BlazorでChart.jsを使ってグラフを描く](https://yamaccu.github.io/tils/20220316-Blazor-Chartjs)
